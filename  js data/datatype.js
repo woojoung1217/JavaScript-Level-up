6642c0729e38344cd96d4b01ba59c0f7deebd0c4
woojoung <ywj981217@naver.com>
2021-08-19 15:21:56 +0900

    new data type

---  js data/datatype.js
+++  js data/datatype.js
@@ -20,7 +20,7 @@ const str = '0123'
 console.log(str.length) //length property 로 문자열의 길이를 알아낼 수 있음.
 console.log('2334211114'.length) //문자열 자체로도 가능
 
-const string = 'Hello world!'
+const string = '  Hello world!  '
 
 console.log(string.indexOf('woojoung') !== -1) //문자열에 내용이 없으면 false 를 반환 
 console.log(string.slice(0 ,3))   //문자 자르기 0 ~3 까지의 문자를 잘라 사용 
@@ -33,3 +33,4 @@ const datastr = 'ywj981217@naver.com'
 
 console.log(datastr.match(/.+(?=@)/)[0]) //정규표현식으로 문자열 매치 시킨다. 
 
+console.log(string)
