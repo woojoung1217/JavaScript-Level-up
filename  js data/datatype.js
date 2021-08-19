b6079d6e5cec214e83782240962c01617b549ab3
woojoung <ywj981217@naver.com>
2021-08-19 15:19:13 +0900

    new data type

---  js data/datatype.js
+++  js data/datatype.js
@@ -24,3 +24,12 @@ const string = 'Hello world!'
 
 console.log(string.indexOf('woojoung') !== -1) //문자열에 내용이 없으면 false 를 반환 
 console.log(string.slice(0 ,3))   //문자 자르기 0 ~3 까지의 문자를 잘라 사용 
+console.log(string.slice(6,11))
+
+console.log(string.replace('world','woojoung'))  //문자열의 내용을 재배치 (바꾸는내용 , 새로 배치될 내용)
+console.log(string.replace(' world!', '')) //문자열 삭제
+
+const datastr = 'ywj981217@naver.com'
+
+console.log(datastr.match(/.+(?=@)/)[0]) //정규표현식으로 문자열 매치 시킨다. 
+
