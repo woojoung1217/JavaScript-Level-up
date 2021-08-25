15a370bc19ffe93eaf7f601d9d50e1a126ec7391
woojoung <ywj981217@naver.com>
2021-08-25 17:41:06 +0900

    2021-08-25

---
+++  js data/Spread.js
@@ -0,0 +1,19 @@
+
+// 전개 연산자 (Spread)
+
+const fruits = ['Apple','Banana','Cherry'] //array data 생성 
+  
+console.log(fruits)  // 출력 
+
+console.log(...fruits)  // array 안의 인덱스 요소의 데이터만 출력 
+ // 출력 결과 : Apple , Banana , Cherry  => 문자데이터만 출력됨 
+
+function toObject(a,b,c) {  //배열데이터를 인수로 받아서 전개 연산자를 통해 출력 
+  return {
+    a:a,
+    b:b,
+    c:c
+  }
+}
+console.log(toObject(...fruits))
+ // 전개 연산자를 사용하면 함수내 매개변수가 지정되어 있지 않아도 ...전개 연산을 사용하면 나머지로 모두 할당 된다.
