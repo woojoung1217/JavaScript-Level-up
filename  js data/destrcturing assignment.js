889444cf48e0b1de64856d947006141689883660
woojoung <ywj981217@naver.com>
2021-08-25 17:23:10 +0900

    2021-08-25

---  js data/destrcturing assignment.js
+++  js data/destrcturing assignment.js
@@ -7,20 +7,22 @@ const user = {
 }
 
 // 구조 분해 할당이란 지정된 객체 데이터 안에서 내가 원하는 객체요소만 꺼내서 사용할 수 있는 것을 말한다.
+     
+const { name, age, email , address='korea'} = user  //선언 [구조분해]
+// 구조분해 시 기본 값을 설정 할 수 있다.
 
-const { name, age, email , address} = user  //선언 [구조분해]
-
+const woojoung = name
 // example 
 
 console.log(`사용자의 이름은 ${name} 입니다.`)  // user 객체의 선언 된 내용이 호출됨 
-console.log(`${name}의 나이는 ${age}세 입니다.`)
+console.log(`${woojoung}의 나이는 ${age}세 입니다.`)
 console.log(`${name}의 이메일 주소는${email} 입니다.`)
-
-
+console.log(address)
 //example 2
 
-const fruits = ['Apple','Banana', 'Cheery'] // 배열데이터의 경우
+const fruits = ['Apple','Banana', 'Cheery','orange'] // 배열데이터의 경우
 
-const [a,b,c,d] = fruits
+const [, ,b] = fruits
 
-console.log(a,b,c,d)  
+console.log(b)  
+
