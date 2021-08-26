2682fad567c8c445c69c2d93a330130c539c868c
woojoung <ywj981217@naver.com>
2021-08-26 16:40:40 +0900

    2021-08-26

---  js data/Immutabilty.js
+++  js data/Immutabilty.js
@@ -3,14 +3,15 @@
 // 원시데이터 : String , Number , Boolean , undefinend , null
 // 참조형데이터 : Object , Array , Function 
 
-let a = 1
-let b = 4
+let a = {k:1}
+let b = {k:1}
 
 console.log(a,b , a === b) // 1:4 false 
+a.k = 7
 b = a                      // a:1 b:1 
 console.log(a,b , a === b)  // 1:1 true
-a = 7
+a.k= 2
 console.log(a,b, a === b) // a: 7 b: 1 false
-let c = 1
+let c = b
 console.log(b,c, b === c) // b: 1 c: 1 true 
 
